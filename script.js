3c02d6e6ffad9d0a5648bcf6e936ecafea206fbc
lizzy-bird <elizabethharris.soprano@gmail.com>
2021-11-11 20:51:35 -0500

    fixed the JS so the listings appear on the page, added SCSS for li and h2 for returned listings, added genre codes to dropdown menu in index file

--- script.js
+++ script.js
@@ -8,20 +8,29 @@ const cinemaHunters = {}
 
 //Set up API key/url✔
 cinemaHunters.apiKey = 'df23e141e5849f2396ad851c2744c80c';
-cinemaHunters.apiUrl = 'https://api.themoviedb.org/3/movie/550';
+cinemaHunters.apiUrl = 'https://api.themoviedb.org/3';
 //add endpoints?
-cinemaHunters.endPoints = 'https://api.themoviedb.org/3/genre/movie/list?api_key=df23e141e5849f2396ad851c2744c80c'
+// cinemaHunters.endPoints = ''
 
 //make a method to define how to get the listings from the API
-cinemaHunters.getMovies = () => {
-    const url = new URL(cinemaHunters.apiUrl);
+cinemaHunters.getMovies = (genreChoice, year) => {
+    const url = new URL(`${cinemaHunters.apiUrl}/discover/movie`);
     url.search= new URLSearchParams({
         api_key: cinemaHunters.apiKey,
-        list: genreChoice
+        with_genres: genreChoice,
+        year: year
+    })
+    //fetch the API using url variable✔
+	//pass call back function to retrieve info from API✔
+	//parse API into JSON to return an array✔
+    fetch (url)
+    .then ((response)=>{
+        return response.json()
+    })
+    .then((jsonResponse) =>{
+        cinemaHunters.displayMovies(jsonResponse.results)
     })
 }
-
-//! How can we build the URL to search specifically for the parameters we are filtering for? Can we use more than one end point at a time?
 
 console.log(cinemaHunters)
 
@@ -29,26 +38,19 @@ console.log(cinemaHunters)
 
 cinemaHunters.setUpEventListeners = () => {
     document.getElementById('genre').addEventListener('change',(event) => {
-        const genreChoice = event.target.value
-        cinemaHunters.displayMovies(genreChoice)
-        
-    })
+        // event.preventDefault();
 
-    //! How do we create a duplicate event listener (one for each dropdown) without duplicating unnecessarily? Does this need to be a filter?
+        const genreChoice = event.target.value
+        // console.log(genreChoice)
+        // console.log(event)
+        const year = event.target.release_date
+        cinemaHunters.getMovies(genreChoice, year);
+    })
     
     
  }
 
-//fetch the API using url variable✔
-	//pass call back function to retrieve info from API✔
-	//parse API into JSON to return an array✔
-fetch (url)
-    .then ((response)=>{
-        return response.json()
-    })
-    .then((jsonResponse) =>{
-        cinemaHunters.displayMovies(jsonResponse)
-    })
+
 
 //filter the JSON array with the user inputs and return only the results matching the selectedyear & genre
 
@@ -65,18 +67,29 @@ fetch (url)
         
         
         arrayDataFromAPI.forEach((movie) => {
-            const li = document.createElement('li');
-            const img = document.createElement('img');
-            img.src = movie.poster_path;
-            img.alt = movie.title;
+            const movieListing = document.createElement('li');
+
+
+            // const movieImage = document.createElement('img');
+            // movieImage.src = `${url}${movie.posterpath}`; //!this isn't showing up - the image is just a jpg, so how can we link to it?
+            // movieImage.alt = movie.title;
             
-            const h2 = document.createElement('h2');
+            const movieTitle = document.createElement('h2');
             movieTitle.innerText = movie.title;
 
-            li.appendChild(img);
-            li.appendChild(h2);
-            ul.appendChild(li);
+            const movieDescription = document.createElement('p');
+            movieDescription.innerText = movie.overview;
 
+            // movieListing.appendChild(movieImage);
+            movieListing.appendChild(movieTitle);
+            movieListing.appendChild(movieDescription);
+
+            //display on the page
+            ul.appendChild(movieListing);
+
+            console.log(movie)
+            // genreChoice.reset();
+            // year.reset();
 
         })
 
@@ -87,11 +100,14 @@ fetch (url)
 //create an init that runs the retrieve, filter and display methods
 
 cinemaHunters.init = () => {
-    cinemaHunters.getMovies();
+    
     //cinemaHunters.filterMovies();
-    cinemaHunters.displayMovies();
+    cinemaHunters.setUpEventListeners()
+
+    //create 
 }
 
+//!need to add a form clearing function
 //call init
 
 cinemaHunters.init();
